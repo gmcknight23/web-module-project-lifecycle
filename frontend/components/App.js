7508658e94286061040365f5741c1cdc980df0ad
gregmcknight <gmcknight23@gmail.com>
2022-03-07 15:13:42 -0500

    0313p

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">Error: no error here</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          <div>Walk the dog</div>
+          <div>Learn React</div>
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    );
   }
 }
